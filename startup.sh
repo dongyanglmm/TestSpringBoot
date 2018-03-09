e9ec6fde1f0de212294e15a33bde0c075c5f3c12
dongyang <dongyang@weshare.com.cn>
2018-03-09 16:46:33 +0800

    init project

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 # startup.sh Æô¶¯ÏîÄ¿
 #!/bin/sh
 chmod 777 /var/jenkins_home/workspace/TestSpringBoot/build/libs/TestSpringBoot-0.0.1-SNAPSHOT.jar
-java -jar /var/jenkins_home/workspace/TestSpringBoot/build/libs/TestSpringBoot-0.0.1-SNAPSHOT.jar
+java -jar /var/jenkins_home/workspace/TestSpringBoot/build/libs/TestSpringBoot-0.0.1-SNAPSHOT.jar &
